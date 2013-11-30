7216f05e0c3797a5bd1dd1d1235d3c1cad6a42f7
jtmelton <jtmelton@gmail.com>
2013-11-29 20:59:38 -0500

    Setting up for stax parsing instead of jaxb

--- appsensor-core/src/main/java/org/owasp/appsensor/package-info.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/package-info.java
@@ -3,5 +3,6 @@
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
+//TODO: this should be able to be removed
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://www.owasp.org/index.php/OWASP_AppSensor_Project/xsd/appsensor_server_config_2.0.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.owasp.appsensor;
