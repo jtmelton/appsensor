62360e64d640b233185faea79d426df6affa8c75
jtmelton <jtmelton@gmail.com>
2013-12-01 17:01:44 -0500

    Updating and adding javadoc.

--- appsensor-core/src/main/java/org/owasp/appsensor/ResponseHandler.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/ResponseHandler.java
@@ -2,7 +2,8 @@ package org.owasp.appsensor;
 
 /**
  * The ResponseHandler is executed when a {@link Response} needs to be executed. 
- * The ResponseHandler is used by the client application
+ * The ResponseHandler is used by the client application, or possibly the server 
+ * side in a local configuration.
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
@@ -14,6 +15,12 @@ public interface ResponseHandler {
 	public final static String DISABLE_COMPONENT_FOR_SPECIFIC_USER = "disableComponentForSpecificUser";
 	public final static String DISABLE_COMPONENT_FOR_ALL_USERS = "disableComponentForAllUsers";
 	
+	/**
+	 * The handle method is called when a given response should be processed. 
+	 * It is the responsibility of the handle method to actually execute the intented response.
+	 * 
+	 * @param response Response object that should be processed
+	 */
 	public void handle(Response response);
 	
 }
