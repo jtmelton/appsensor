03467309c2c291ad6c70f3f88d76da9d10bea8c5
jtmelton <jtmelton@gmail.com>
2013-12-02 15:40:42 -0500

    move from jaxb to stax

--- appsensor-local/src/test/java/org/owasp/appsensor/configuration/XmlClientConfigurationReaderTest.java
+++ appsensor-local/src/test/java/org/owasp/appsensor/configuration/XmlClientConfigurationReaderTest.java
@@ -5,15 +5,22 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.owasp.appsensor.configuration.client.ClientConfiguration;
 import org.owasp.appsensor.configuration.client.ClientConfigurationReader;
-import org.owasp.appsensor.configuration.client.XmlClientConfigurationReader;
+import org.owasp.appsensor.configuration.client.StaxClientConfigurationReader;
 
 public class XmlClientConfigurationReaderTest {
 	
 	@Test
 	public void testConfigLoad() throws Exception {
-		ClientConfigurationReader reader = new XmlClientConfigurationReader();
+		ClientConfigurationReader reader = new StaxClientConfigurationReader();
 		ClientConfiguration configuration = reader.read("/appsensor-client-config.xml", "/appsensor_client_config_2.0.xsd");
 		
 		assertTrue("org.owasp.appsensor.event.impl.LocalEventManager".equals(configuration.getEventManagerImplementation()));
+		assertTrue("org.owasp.appsensor.response.impl.NoopResponseHandler".equals(configuration.getResponseHandlerImplementation()));
+		assertTrue("org.owasp.appsensor.user.impl.NoopUserManager".equals(configuration.getUserManagerImplementation()));
+		assertTrue("rest".equals(configuration.getServerConnection().getType()));
+		assertTrue("https".equals(configuration.getServerConnection().getProtocol()));
+		assertTrue("www.owasp.org".equals(configuration.getServerConnection().getHost()));
+		assertTrue(5000 == configuration.getServerConnection().getPort());
+		assertTrue("/appsensor/v2/api/".equals(configuration.getServerConnection().getPath()));
 	}
 }
--- appsensor-local/src/test/java/org/owasp/appsensor/configuration/XmlServerConfigurationReaderTest.java
+++ appsensor-local/src/test/java/org/owasp/appsensor/configuration/XmlServerConfigurationReaderTest.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.owasp.appsensor.configuration.server.ServerConfiguration;
 import org.owasp.appsensor.configuration.server.ServerConfigurationReader;
-import org.owasp.appsensor.configuration.server.XmlServerConfigurationReader;
+import org.owasp.appsensor.configuration.server.StaxServerConfigurationReader;
 
 public class XmlServerConfigurationReaderTest {
 	
 	@Test
 	public void testConfigLoad() throws Exception {
-		ServerConfigurationReader reader = new XmlServerConfigurationReader();
+		ServerConfigurationReader reader = new StaxServerConfigurationReader();
 		ServerConfiguration configuration = reader.read("/appsensor-server-config.xml", "/appsensor_server_config_2.0.xsd");
 		
 		assertTrue("org.owasp.appsensor.analysis.impl.ReferenceStatisticalEventAnalysisEngine".equals(configuration.getEventAnalysisEngineImplementation()));
