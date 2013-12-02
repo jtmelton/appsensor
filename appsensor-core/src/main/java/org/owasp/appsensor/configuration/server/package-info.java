d8a3d18857059b363483cab97dafd448649c7f88
jtmelton <jtmelton@gmail.com>
2013-12-02 15:11:51 -0500

    adding future removal comment reminder.

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/server/package-info.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/server/package-info.java
@@ -1,2 +1,3 @@
+//TODO: this may be removed if we get rid of the jaxb processing altogether
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://www.owasp.org/index.php/OWASP_AppSensor_Project/xsd/appsensor_server_config_2.0.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.owasp.appsensor.configuration.server;
