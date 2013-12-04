9445f7ffc6ec8dcdc75d640c4a4d115a7304a79d
jtmelton <jtmelton@gmail.com>
2013-12-03 20:23:35 -0500

    Adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/server/ServerConfigurationReader.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/server/ServerConfigurationReader.java
@@ -2,8 +2,19 @@ package org.owasp.appsensor.configuration.server;
 
 import java.text.ParseException;
 
-
-
+/**
+ * This interface is to be fulfilled by implementations that load a configuration 
+ * file and provide an object representation of it. 
+ * 
+ * The current implementation only consists of an XML configuration that utilizes a 
+ * standardized XSD schema. However, there is nothing in the interface requiring the 
+ * XML implementation. Most standard users will likely stick to the standard implementation. 
+ * 
+ * TODO: may update this interface is we move to something other than "reading" 
+ * the config, ie. supporting configs from data stores/cloud, etc.
+ * 
+ * @author johnmelton
+ */
 public interface ServerConfigurationReader {
 	
 	/**
