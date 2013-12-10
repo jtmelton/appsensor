b0fede9780e69c6ad629b22c346b3eecbf686ec9
jtmelton <jtmelton@gmail.com>
2013-12-10 13:44:36 -0500

    Adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/logging/impl/NoopLogger.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/logging/impl/NoopLogger.java
@@ -7,43 +7,79 @@ package org.owasp.appsensor.logging.impl;
  */
 public class NoopLogger implements org.owasp.appsensor.Logger {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public org.owasp.appsensor.Logger setLoggerClass(String className) {
 		return null;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public org.owasp.appsensor.Logger setLoggerClass(Class<?> clazz) {
 		return null;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void fatal(String message) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void fatal(String message, Throwable throwable) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void error(String message) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void error(String message, Throwable throwable) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void warning(String message) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void warning(String message, Throwable throwable) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void info(String message) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void info(String message, Throwable throwable) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void debug(String message) { }
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void debug(String message, Throwable throwable) { }
 
