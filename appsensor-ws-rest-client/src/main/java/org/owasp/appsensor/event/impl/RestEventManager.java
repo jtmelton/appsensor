79a8cbc2e53ec442803f88d804098effcee5bd58
jtmelton <jtmelton@gmail.com>
2013-12-19 15:07:50 -0500

    adding javadoc

--- appsensor-ws-rest-client/src/main/java/org/owasp/appsensor/event/impl/RestEventManager.java
+++ appsensor-ws-rest-client/src/main/java/org/owasp/appsensor/event/impl/RestEventManager.java
@@ -8,23 +8,33 @@ import org.owasp.appsensor.EventManager;
 import org.owasp.appsensor.Response;
 
 /**
+ * This event manager should perform rest style requests since it functions
+ * as the reference rest client.
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
- *
  */
 public class RestEventManager implements EventManager {
 
 	//TODO: do a rest request based on configuration 
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void addEvent(Event event) {
 		//make request
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public void addAttack(Attack attack) {
 		//make request
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Collection<Response> getResponses() {
 		//make request
