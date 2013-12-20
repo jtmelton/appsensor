3ebb2cdd29098b1274e68f0eced1c18fbcb0348b
jtmelton <jtmelton@gmail.com>
2013-12-20 15:21:40 -0500

    Adding javadoc

--- appsensor-ws-soap-client/src/main/java/org/owasp/appsensor/event/impl/SoapEventManager.java
+++ appsensor-ws-soap-client/src/main/java/org/owasp/appsensor/event/impl/SoapEventManager.java
@@ -8,23 +8,34 @@ import org.owasp.appsensor.EventManager;
 import org.owasp.appsensor.Response;
 
 /**
+ * This event manager should perform soap style requests since it functions
+ * as the reference soap client.
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
- *
  */
 public class SoapEventManager implements EventManager {
 
 	//TODO: do a soap request based on configuration 
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void addEvent(Event event) {
 		//make request
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public void addAttack(Attack attack) {
 		//make request
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Collection<Response> getResponses() {
 		//make request
