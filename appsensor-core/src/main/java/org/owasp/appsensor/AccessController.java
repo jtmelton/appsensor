ab7a99c5c15d794f22d91fae82d2bdafb4b60a76
jtmelton <jtmelton@gmail.com>
2013-12-26 22:34:39 -0500

    updating todos in javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/AccessController.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/AccessController.java
@@ -3,6 +3,8 @@ package org.owasp.appsensor;
 /**
  * This interface is meant to gate access to the different actions 
  * that can be performed to ensure a client has appropriate permissions.
+ * 
+ * TODO: this interface may be removed or modified depending on how access control is implemented
  *
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
