b236d2a323188dc3dd89bbddab433fafbef66051
jtmelton <jtmelton@gmail.com>
2013-12-28 21:20:23 -0500

    test shell for soap client

---
+++ appsensor-ws-soap-client/src/test/java/org/owasp/appsensor/event/impl/SoapEventManagerTest.java
@@ -0,0 +1,30 @@
+package org.owasp.appsensor.event.impl;
+
+import org.junit.Test;
+
+public class SoapEventManagerTest {
+
+//	private static String SERVICE_URL = "http://localhost:8080/appsensor/services/SoapRequestHandler";
+    @Test
+    public void test() throws Exception {
+//		System.err.println("Starting service");
+//    	Endpoint endpoint = Endpoint.publish(SERVICE_URL, new ReferenceSoapRequestHandler());
+//		try {
+//			Thread.sleep(10000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		endpoint.stop();
+//        Service calculatorService = Service.create(
+//                new URL("http://127.0.0.1:4204/Calculator?wsdl"),
+//                new QName("http://superbiz.org/wsdl", "CalculatorService"));
+//
+//        
+//        assertNotNull(calculatorService);
+//
+//        CalculatorWs calculator = calculatorService.getPort(CalculatorWs.class);
+//        assertEquals(10, calculator.sum(4, 6));
+//        assertEquals(12, calculator.multiply(3, 4));
+    }
+}
