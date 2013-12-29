fbcbabc40694cebb918ed1870a2ecf0df906f14e
jtmelton <jtmelton@gmail.com>
2013-12-29 09:15:03 -0500

    updated javadoc and added logging

--- appsensor-local/src/main/java/org/owasp/appsensor/user/impl/NoopUserManager.java
+++ appsensor-local/src/main/java/org/owasp/appsensor/user/impl/NoopUserManager.java
@@ -1,18 +1,37 @@
 package org.owasp.appsensor.user.impl;
 
+import org.owasp.appsensor.AppSensorServer;
+import org.owasp.appsensor.Logger;
 import org.owasp.appsensor.User;
 import org.owasp.appsensor.UserManager;
+import org.owasp.appsensor.event.impl.LocalEventManager;
 
+/**
+ * No-op user manager that is used most likely in test configurations. 
+ * It is possible the response handler could handle these actions 
+ * directly, but unlikely. 
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ *
+ */
 public class NoopUserManager implements UserManager {
 
+	private static Logger logger = AppSensorServer.getInstance().getLogger().setLoggerClass(LocalEventManager.class);
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void logout(User user) {
-		//do nothing
+		logger.info("The no-op user manager did not logout the user as requested.");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void disable(User user) {
-		//do nothing
+		logger.info("The no-op user manager did not disable the user as requested.");
 	}
 
 }
