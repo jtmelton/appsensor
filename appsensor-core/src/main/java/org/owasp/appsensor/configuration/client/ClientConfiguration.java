5428069a3c38d2355426f5ff70d27aad9ed029b1
jtmelton <jtmelton@gmail.com>
2014-01-04 10:10:09 -0500

    adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/client/ClientConfiguration.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/client/ClientConfiguration.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+/**
+ * Represents the configuration for client-side components. 
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public class ClientConfiguration {
 
 	private String eventManagerImplementation;
