fbdc315d5ecde553c1f281158ee1e9404456cd5b
jtmelton <jtmelton@gmail.com>
2014-01-20 22:28:07 -0500

    adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/response/ResponseHandler.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/response/ResponseHandler.java
@@ -17,6 +17,7 @@ public interface ResponseHandler {
 	public final static String LOGOUT = "logout"; 
 	/** disable this specific user */
 	public final static String DISABLE_USER = "disableUser";
+	/** disable a component for this specific user */
 	public final static String DISABLE_COMPONENT_FOR_SPECIFIC_USER = "disableComponentForSpecificUser";
 	public final static String DISABLE_COMPONENT_FOR_ALL_USERS = "disableComponentForAllUsers";
 	
