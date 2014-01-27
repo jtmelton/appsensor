861a362eb34a7419b465cab282482d050f829242
jtmelton <jtmelton@gmail.com>
2014-01-27 11:23:34 -0500

    adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/exceptions/ConfigurationException.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/exceptions/ConfigurationException.java
@@ -1,5 +1,10 @@
 package org.owasp.appsensor.exceptions;
 
+/**
+ * This exception is for anytime the configuration for appsensor is invalid.
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public class ConfigurationException extends RuntimeException  {
 
 	private static final long serialVersionUID = 538520201225584981L;
