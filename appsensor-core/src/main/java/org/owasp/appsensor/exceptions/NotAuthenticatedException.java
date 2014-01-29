3950c4ab45e4f082f14db383145563012b97bd0e
jtmelton <jtmelton@gmail.com>
2014-01-29 11:14:22 -0500

    adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/exceptions/NotAuthenticatedException.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/exceptions/NotAuthenticatedException.java
@@ -1,5 +1,12 @@
 package org.owasp.appsensor.exceptions;
 
+/**
+ * This exception is meant to be thrown by the {@link org.owasp.appsensor.RequestHandler}
+ * when a {@link org.owasp.appsensor.ClientApplication} is not providing appropriate
+ * authentication credentials
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public class NotAuthenticatedException extends RuntimeException  {
 
 	private static final long serialVersionUID = 538520201225584981L;
