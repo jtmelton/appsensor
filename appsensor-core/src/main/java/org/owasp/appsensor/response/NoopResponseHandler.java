207afdae7e1f7a4b81f81afc32439d896fa499a8
jtmelton <jtmelton@gmail.com>
2014-02-21 03:05:33 -0500

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/response/NoopResponseHandler.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/response/NoopResponseHandler.java
@@ -1,11 +1,11 @@
 package org.owasp.appsensor.response;
 
-import org.owasp.appsensor.Response;
 import org.owasp.appsensor.AppSensorServer;
+import org.owasp.appsensor.Response;
 import org.owasp.appsensor.logging.Logger;
 
 /**
- * This class is a simple NO-OP {@link org.owasp.appsensor.Response} handler. Calls to this {@link org.owasp.appsensor.Response} handler simply 
+ * This class is a simple NO-OP {@link Response} handler. Calls to this {@link Response} handler simply 
  * log the action to the configured logger. 
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
