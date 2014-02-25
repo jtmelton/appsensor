1016b576cd08869f8b8b48fca16875ca39007f3d
jtmelton <jtmelton@gmail.com>
2014-02-25 12:03:55 -0500

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/event/EventManager.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/event/EventManager.java
@@ -3,11 +3,12 @@ package org.owasp.appsensor.event;
 import java.util.Collection;
 
 import org.owasp.appsensor.Attack;
+import org.owasp.appsensor.ClientApplication;
 import org.owasp.appsensor.Event;
 import org.owasp.appsensor.Response;
 
 /**
- * The EventManager is the key interface that the client application accesses to 
+ * The EventManager is the key interface that the {@link ClientApplication} accesses to 
  * interact with AppSensor.
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
@@ -15,23 +16,23 @@ import org.owasp.appsensor.Response;
 public interface EventManager {
 	
 	/**
-	 * Add an Event.
+	 * Add an {@link Event}.
 	 * 
-	 * @param event Event to add
+	 * @param event {@link Event} to add
 	 */
 	public void addEvent(Event event);
 	
 	/**
-	 * Add an Attack
-	 * @param attack Attack to add
+	 * Add an {@link Attack}
+	 * @param attack {@link Attack} to add
 	 */
 	public void addAttack(Attack attack);
 
 	/**
-	 * Retrieve any responses generated that apply to this 
+	 * Retrieve any {@link Response}s generated that apply to this 
 	 * client since the last time the client called this method.
 	 *  
-	 * @return a Collection of Response objects 
+	 * @return a Collection of {@link Response} objects 
 	 */
 	public Collection<Response> getResponses();
 }
