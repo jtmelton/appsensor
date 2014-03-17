a0cbe6c0c033e7b8f7e8d00c07729f7d563a683d
jtmelton <jtmelton@gmail.com>
2014-03-17 00:16:36 -0400

    Simple rest reporting engine

---
+++ appsensor-ws-rest-server/src/main/java/org/owasp/appsensor/reporting/RestReportingEngine.java
@@ -0,0 +1,88 @@
+package org.owasp.appsensor.reporting;
+
+import java.util.Collection;
+import java.util.Observable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.owasp.appsensor.AppSensorServer;
+import org.owasp.appsensor.Attack;
+import org.owasp.appsensor.Event;
+import org.owasp.appsensor.Response;
+import org.owasp.appsensor.accesscontrol.Action;
+import org.owasp.appsensor.criteria.SearchCriteria;
+import org.owasp.appsensor.exceptions.NotAuthorizedException;
+import org.owasp.appsensor.rest.AccessControlUtils;
+
+/**
+ * This is the restful endpoint that handles reporting requests on the server-side. 
+ * 
+ * This simple RESTful implementation queries the appropriate *Store implementations 
+ * for matching entities.
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
+@Path("/api/v1.0/reports")
+@Produces(MediaType.APPLICATION_JSON)
+public class RestReportingEngine implements ReportingEngine {
+
+	@Context
+	private ContainerRequestContext requestContext;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		//ignore - don't need updates - not collecting notifications here
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@GET
+	@Path("/events")
+	public Collection<Event> findEvents(@QueryParam("earliest") Long earliest) throws NotAuthorizedException {
+		AccessControlUtils.checkAuthorization(Action.EXECUTE_REPORT, requestContext);
+		
+		SearchCriteria criteria = new SearchCriteria().setEarliest(earliest);
+		
+		return AppSensorServer.getInstance().getEventStore().findEvents(criteria);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@GET
+	@Path("/attacks")
+	public Collection<Attack> findAttacks(@QueryParam("earliest") Long earliest) {
+		AccessControlUtils.checkAuthorization(Action.EXECUTE_REPORT, requestContext);
+		
+		SearchCriteria criteria = new SearchCriteria().setEarliest(earliest);
+		
+		return AppSensorServer.getInstance().getAttackStore().findAttacks(criteria);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@GET
+	@Path("/responses")
+	public Collection<Response> findResponses(@QueryParam("earliest") Long earliest) {
+		AccessControlUtils.checkAuthorization(Action.EXECUTE_REPORT, requestContext);
+		
+		SearchCriteria criteria = new SearchCriteria().setEarliest(earliest);
+		
+		return AppSensorServer.getInstance().getResponseStore().findResponses(criteria);
+	}
+	
+}
