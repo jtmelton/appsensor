37a81c9a67b4464e61258a975a6d07518b4ef54e
jtmelton <jtmelton@gmail.com>
2014-03-18 00:49:18 -0400

    adding javadoc

--- appsensor-ws-rest-server/src/main/java/org/owasp/appsensor/rest/AppSensorApplication.java
+++ appsensor-ws-rest-server/src/main/java/org/owasp/appsensor/rest/AppSensorApplication.java
@@ -9,6 +9,13 @@ import org.owasp.appsensor.handler.RestRequestHandler;
 import org.owasp.appsensor.reporting.RestReportingEngine;
 import org.owasp.appsensor.rest.filter.ClientApplicationIdentificationFilter;
 
+/**
+ * This JAX-RS class provides the runtime with the classes that need to
+ * be managed, allowing {@link javax.ws.rs.core.Context} injections 
+ * to be made on the exposed classes.  
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public class AppSensorApplication extends Application {
 	
     @Override
