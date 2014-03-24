8168e1a7576046bfa77f0662ec25ab31b4475e17
jtmelton <jtmelton@gmail.com>
2014-03-23 20:40:56 -0400

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/reporting/ReportingEngine.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/reporting/ReportingEngine.java
@@ -11,15 +11,14 @@ import org.owasp.appsensor.listener.EventListener;
 import org.owasp.appsensor.listener.ResponseListener;
 
 /**
- * A reporting engine is an implementation of the observer pattern. 
+ * A reporting engine is an implementation of the observer pattern and 
+ * extends the *Listener interfaces. 
  * 
  * In this case the reporting engines watch the *Store interfaces of AppSensor.
  * 
  * The reporting engines are meant to provide simple access to get notified 
  * when the different components are added to the *Store's for reporting.
  * 
- * @see java.util.Observer
- *
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
 public interface ReportingEngine extends EventListener, AttackListener, ResponseListener {
