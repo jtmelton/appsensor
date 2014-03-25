cbdfcc994310db8effefa439897b938b665049bd
jtmelton <jtmelton@gmail.com>
2014-03-25 14:15:52 -0400

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/listener/EventListener.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/listener/EventListener.java
@@ -2,6 +2,12 @@ package org.owasp.appsensor.listener;
 
 import org.owasp.appsensor.Event;
 
+/**
+ * This interface is implemented by classes that want to be notified
+ * when a new {@link Event} is created and stored in the AppSensor system. 
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public interface EventListener {
 	public void onAdd(Event event);
 }
