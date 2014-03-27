d7d659adbd8402a988dec956e2a7cb2ccb7aca7a
John Melton <jtmelton@gmail.com>
2014-03-27 00:40:55 -0400

    adding javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/listener/AttackListener.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/listener/AttackListener.java
@@ -1,7 +1,22 @@
 package org.owasp.appsensor.listener;
 
 import org.owasp.appsensor.Attack;
+import org.owasp.appsensor.storage.AttackStore;
 
+/**
+ * This interface is implemented by classes that want to be notified
+ * when a new {@link Attack} is created and stored in the AppSensor system. 
+ * 
+ * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+ */
 public interface AttackListener {
+	
+	/**
+	 * Listener method to handle when a new 
+	 * {@link AttackStore} is added to the {@link AttackStore}
+	 * 
+	 * @param attack {@link AttackStore} that is added to the {@link AttackStore}
+	 */
 	public void onAdd(Attack attack);
+	
 }
