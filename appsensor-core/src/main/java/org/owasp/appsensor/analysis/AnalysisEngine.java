f45a993af186a6774843495303f0e146988b27cb
John Melton <jtmelton@gmail.com>
2014-03-29 00:42:25 -0400

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/analysis/AnalysisEngine.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/analysis/AnalysisEngine.java
@@ -4,15 +4,13 @@ package org.owasp.appsensor.analysis;
 /**
  * An analysis engine is an implementation of the Observer pattern. 
  * 
- * It watches implementations of the {@link java.util.Observable} interface. 
+ * It watches implementations of the *Listener interface. 
  * 
  * In this case the analysis engines watch the *Store interfaces of AppSensor.
  * 
  * AnalysisEngine implementations are the components of AppSensor that 
  * constitute the "brain" of the system. 
  * 
- * @see java.util.Observer
- *
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
 public interface AnalysisEngine {
