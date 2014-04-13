0c564c31843092febae6f49b60b501293dca320c
jtmelton <jtmelton@gmail.com>
2014-04-12 22:01:18 -0400

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/ExtendedConfiguration.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/ExtendedConfiguration.java
@@ -13,12 +13,23 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class ExtendedConfiguration {
 
+	/** 
+	 * Collection of {@link ConfigurationParameter}s to
+	 * use to extend default object construction behavior 
+	 */
 	public Collection<ConfigurationParameter> configurationParameters = new HashSet<>();
 	
 	public Collection<ConfigurationParameter> getConfigurationParameters() {
 		return configurationParameters;
 	}
 	
+	/**
+	 * Retrieve value from {@link ConfigurationParameter} based on key
+	 * 
+	 * @param key search key
+	 * @param defaultValue default value to use if key is not found
+	 * @return value matching key, or default value if key is not found
+	 */
 	public String findValue(String key, String defaultValue) {
 		String value = defaultValue;
 		
