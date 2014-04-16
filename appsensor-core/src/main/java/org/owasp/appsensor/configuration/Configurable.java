47dc767e81e5d11c629ab0242c998f6ab5468f34
jtmelton <jtmelton@gmail.com>
2014-04-16 00:12:06 -0400

    updating javadoc

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/Configurable.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/Configurable.java
@@ -8,7 +8,16 @@ package org.owasp.appsensor.configuration;
  */
 public interface Configurable {
 
+	/**
+	 * Retrieve the {@link ExtendedConfiguration} object.
+	 * @return the {@link ExtendedConfiguration} object
+	 */
 	public ExtendedConfiguration getExtendedConfiguration();
+	
+	/**
+	 * Set the {@link ExtendedConfiguration} object.
+	 * @param extendedConfiguration the {@link ExtendedConfiguration} object
+	 */
 	public void setExtendedConfiguration(ExtendedConfiguration extendedConfiguration);
 	
 }
