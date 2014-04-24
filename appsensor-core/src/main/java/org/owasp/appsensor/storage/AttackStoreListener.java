70d6cfe1721b6a26ebce2d147389c242ab8ddfa1
jtmelton <jtmelton@gmail.com>
2014-04-24 00:35:30 -0400

    new annotations for listeners

---
+++ appsensor-core/src/main/java/org/owasp/appsensor/storage/AttackStoreListener.java
@@ -0,0 +1,19 @@
+package org.owasp.appsensor.storage;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
+public @interface AttackStoreListener {
+
+}
