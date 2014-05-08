dc645b27bc2145f7eb985e1a952df9515210c81f
jtmelton <jtmelton@gmail.com>
2014-05-08 01:32:01 -0400

    making *StoreListeners @Inherited so you don't have to specify it as long as you extend one of the *AnalysisEngine interfaces.

--- appsensor-core/src/main/java/org/owasp/appsensor/storage/ResponseStoreListener.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/storage/ResponseStoreListener.java
@@ -6,6 +6,7 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -14,6 +15,7 @@ import javax.inject.Qualifier;
 @Qualifier
 @Retention(RUNTIME)
 @Target({TYPE, METHOD, FIELD, PARAMETER})
+@Inherited
 public @interface ResponseStoreListener {
 
 }
