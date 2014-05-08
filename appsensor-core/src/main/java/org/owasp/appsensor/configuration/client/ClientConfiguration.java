2720d50d2c1292a03c0a7ec56b71b592d05fe7ee
jtmelton <jtmelton@gmail.com>
2014-05-08 01:34:39 -0400

    full overhaul to use DI

--- appsensor-core/src/main/java/org/owasp/appsensor/configuration/client/ClientConfiguration.java
+++ appsensor-core/src/main/java/org/owasp/appsensor/configuration/client/ClientConfiguration.java
@@ -3,7 +3,6 @@ package org.owasp.appsensor.configuration.client;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.owasp.appsensor.configuration.ExtendedConfiguration;
 
 /**
  * Represents the configuration for client-side components. 
@@ -12,81 +11,9 @@ import org.owasp.appsensor.configuration.ExtendedConfiguration;
  */
 public class ClientConfiguration {
 
-	/** Event manager for client-code to interact with appsensor */
-	private String eventManagerImplementation;
-
-	/** Extended configuration for event manager */
-	private ExtendedConfiguration eventManagerExtendedConfiguration;
-	
-	/** Response handler */
-	private String responseHandlerImplementation;
-	
-	/** Extended configuration for response handler */
-	private ExtendedConfiguration responseHandlerExtendedConfiguration;
-	
-	/** User manager for performing responses on users */
-	private String userManagerImplementation;
-	
-	/** Extended configuration for user manager */
-	private ExtendedConfiguration userManagerExtendedConfiguration;
-	
 	/** Server connection with configuration info for rest/soap connections */
 	private ServerConnection serverConnection;
 	
-	public String getEventManagerImplementation() {
-		return eventManagerImplementation;
-	}
-
-	public ClientConfiguration setEventManagerImplementation(String eventManagerImplementation) {
-		this.eventManagerImplementation = eventManagerImplementation;
-		return this;
-	}
-	
-	public ExtendedConfiguration getEventManagerExtendedConfiguration() {
-		return eventManagerExtendedConfiguration;
-	}
-
-	public ClientConfiguration setEventManagerExtendedConfiguration(ExtendedConfiguration eventManagerExtendedConfiguration) {
-		this.eventManagerExtendedConfiguration = eventManagerExtendedConfiguration;
-		return this;
-	}
-
-	public String getResponseHandlerImplementation() {
-		return responseHandlerImplementation;
-	}
-
-	public ClientConfiguration setResponseHandlerImplementation(String responseHandlerImplementation) {
-		this.responseHandlerImplementation = responseHandlerImplementation;
-		return this;
-	}
-	
-	public ExtendedConfiguration getResponseHandlerExtendedConfiguration() {
-		return responseHandlerExtendedConfiguration;
-	}
-
-	public ClientConfiguration setResponseHandlerExtendedConfiguration(ExtendedConfiguration responseHandlerExtendedConfiguration) {
-		this.responseHandlerExtendedConfiguration = responseHandlerExtendedConfiguration;
-		return this;
-	}
-	
-	public String getUserManagerImplementation() {
-		return userManagerImplementation;
-	}
-
-	public ClientConfiguration setUserManagerImplementation(String userManagerImplementation) {
-		this.userManagerImplementation = userManagerImplementation;
-		return this;
-	}
-
-	public ExtendedConfiguration getUserManagerExtendedConfiguration() {
-		return userManagerExtendedConfiguration;
-	}
-
-	public ClientConfiguration setUserManagerExtendedConfiguration(ExtendedConfiguration userManagerExtendedConfiguration) {
-		this.userManagerExtendedConfiguration = userManagerExtendedConfiguration;
-		return this;
-	}
-
 	public ServerConnection getServerConnection() {
 		return serverConnection;
 	}
@@ -99,9 +26,6 @@ public class ClientConfiguration {
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder(17,31).
-				append(eventManagerImplementation).
-				append(responseHandlerImplementation).
-				append(userManagerImplementation).
 				append(serverConnection).
 				toHashCode();
 	}
@@ -118,9 +42,6 @@ public class ClientConfiguration {
 		ClientConfiguration other = (ClientConfiguration) obj;
 		
 		return new EqualsBuilder().
-				append(eventManagerImplementation, other.getEventManagerImplementation()).
-				append(responseHandlerImplementation, other.getResponseHandlerImplementation()).
-				append(userManagerImplementation, other.getUserManagerImplementation()).
 				append(serverConnection, other.getServerConnection()).
 				isEquals();
 	}
@@ -128,9 +49,6 @@ public class ClientConfiguration {
 	@Override
 	public String toString() {
 		return new ToStringBuilder(this).
-				append("eventManagerImplementation", eventManagerImplementation).
-				append("responseHandlerImplementation", responseHandlerImplementation).
-				append("userManagerImplementation", userManagerImplementation).
 				append("serverConnection", serverConnection).
 			    toString();
 	}
