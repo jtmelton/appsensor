9f60c44fb4b97d2a3cae51e93c645fea6620c446
jtmelton <jtmelton@gmail.com>
2014-06-26 23:18:28 -0400

    Remove execution of response from analysis engine. This should be done client-side, not server side.

--- analysis-engines/appsensor-analysis-reference/src/main/java/org/owasp/appsensor/analysis/ReferenceResponseAnalysisEngine.java
+++ analysis-engines/appsensor-analysis-reference/src/main/java/org/owasp/appsensor/analysis/ReferenceResponseAnalysisEngine.java
@@ -1,12 +1,9 @@
 package org.owasp.appsensor.analysis;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.owasp.appsensor.AppSensorClient;
 import org.owasp.appsensor.Response;
 import org.owasp.appsensor.logging.Loggable;
-import org.owasp.appsensor.response.ResponseHandler;
 import org.owasp.appsensor.storage.ResponseStore;
 import org.slf4j.Logger;
 
@@ -16,8 +13,7 @@ import org.slf4j.Logger;
  * 
  * It is notified with implementations of the {@link Response} class.
  * 
- * The implementation is trivial and simply delegates the work to the configured 
- * {@link ResponseHandler} for processing.
+ * The implementation is trivial and simply logs the {@link Response}
  * 
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
@@ -27,21 +23,15 @@ public class ReferenceResponseAnalysisEngine extends ResponseAnalysisEngine {
 
 	private Logger logger;
 	
-	@Inject
-	private AppSensorClient appSensorClient;
-	
 	/**
-	 * This method simply catches responses and calls the 
-	 * configured {@link ResponseHandler} to process them. 
+	 * This method simply logs responses.
 	 * 
 	 * @param response {@link Response} that has been added to the {@link ResponseStore}.
 	 */
 	@Override
 	public void analyze(Response response) {
 		if (response != null) {
-			logger.info("Response executed for user <" + response.getUser().getUsername() + "> - executing response action " + response.getAction());
-			
-			appSensorClient.getResponseHandler().handle(response);
+			logger.info("NO-OP Response for user <" + response.getUser().getUsername() + "> - should be executing response action " + response.getAction());
 		}
 	}
 	
