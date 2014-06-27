67852b5189b344d3b81c1b06eb63ebc556e490f5
jtmelton <jtmelton@gmail.com>
2014-06-26 23:19:45 -0400

    Log warning if client config is missing, don't throw exception.

--- configuration-modes/appsensor-configuration-stax/src/main/java/org/owasp/appsensor/configuration/client/StaxClientConfiguration.java
+++ configuration-modes/appsensor-configuration-stax/src/main/java/org/owasp/appsensor/configuration/client/StaxClientConfiguration.java
@@ -3,6 +3,8 @@ package org.owasp.appsensor.configuration.client;
 import javax.inject.Named;
 
 import org.owasp.appsensor.exceptions.ConfigurationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 
 /**
@@ -12,16 +14,25 @@ import org.springframework.beans.BeanUtils;
  */
 @Named
 public class StaxClientConfiguration extends ClientConfiguration {
-
+	
+	//This needs to be manually setup (not @Loggable) b/c it's used in constructor
+	//so the post-processor won't run before it's used - will get NPE
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	public StaxClientConfiguration() {
 		this(true);
 	}
 	
 	public StaxClientConfiguration(boolean loadConfiguration) {
 		if (loadConfiguration) {
-			ClientConfiguration configuration = loadConfiguration(new StaxClientConfigurationReader());
-			if (configuration != null) {
-				BeanUtils.copyProperties(configuration, this);
+			try {
+				ClientConfiguration configuration = loadConfiguration(new StaxClientConfigurationReader());
+				
+				if (configuration != null) {
+					BeanUtils.copyProperties(configuration, this);
+				}
+			} catch (ConfigurationException e) {
+				logger.warn("Could not load appsensor client configuration file.", e);
 			}
 		}
 	}
@@ -35,14 +46,8 @@ public class StaxClientConfiguration extends ClientConfiguration {
 	 * 
 	 * @param configurationReader desired configuration reader 
 	 */
-	private ClientConfiguration loadConfiguration(ClientConfigurationReader configurationReader) {
-		ClientConfiguration configuration = null;
-		
-		try {
-			configuration = configurationReader.read();
-		} catch(ConfigurationException pe) {
-			throw new RuntimeException(pe);
-		}
+	private ClientConfiguration loadConfiguration(ClientConfigurationReader configurationReader) throws ConfigurationException {
+		ClientConfiguration configuration = configurationReader.read();
 		
 		return configuration;
 	}
