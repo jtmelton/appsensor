433c395df1132f925c499843478b9f21c20965ae
jtmelton <jtmelton@gmail.com>
2015-06-18 23:16:40 -0400

    updates to ui

--- appsensor-ui/src/main/java/org/owasp/appsensor/AppsensorUiApplication.java
+++ appsensor-ui/src/main/java/org/owasp/appsensor/AppsensorUiApplication.java
@@ -2,6 +2,11 @@ package org.owasp.appsensor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class AppsensorUiApplication {
@@ -9,4 +14,23 @@ public class AppsensorUiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppsensorUiApplication.class, args);
     }
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return new AppSensorErrorCustomizer();
+    }
+
+    private static class AppSensorErrorCustomizer implements EmbeddedServletContainerCustomizer {
+
+        @Override
+        public void customize(ConfigurableEmbeddedServletContainer container) {
+        	ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html");
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+ 
+            container.addErrorPages(error400Page, error401Page, error404Page, error500Page);
+        }
+    }
+    
 }
--- appsensor-ui/src/test/java/org/owasp/appsensor/AppsensorUiApplicationTests.java
+++ appsensor-ui/src/test/java/org/owasp/appsensor/AppsensorUiApplicationTests.java
@@ -1,18 +1,58 @@
 package org.owasp.appsensor;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AppsensorUiApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port=0")
+@DirtiesContext
 public class AppsensorUiApplicationTests {
 
+	@Value("${local.server.port}")
+	private int port;
+
 	@Test
-	public void contextLoads() {
+	public void testMustacheTemplate() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port, String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body:\n" + entity.getBody(),
+				entity.getBody().contains("Hello, Andy"));
+	}
+
+	@Test
+	public void testMustacheErrorTemplate() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
+
+		ResponseEntity<String> responseEntity = new TestRestTemplate().exchange(
+				"http://localhost:" + this.port + "/does-not-exist", HttpMethod.GET,
+				requestEntity, String.class);
+
+		assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+		assertTrue("Wrong body:\n" + responseEntity.getBody(), responseEntity.getBody()
+				.contains("Something went wrong: 404 Not Found"));
 	}
 
 }
