99b1b2c41a9dd9b7187776b8c79aced86862bf2b
jtmelton <jtmelton@gmail.com>
2015-06-18 23:09:40 -0400

    helper to encode context path to make it accessible to mustache template

---
+++ appsensor-ui/src/main/java/org/owasp/appsensor/ui/interceptor/ContextPathInterceptor.java
@@ -0,0 +1,35 @@
+package org.owasp.appsensor.ui.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class ContextPathInterceptor extends HandlerInterceptorAdapter {
+
+    /**
+     * The attribute to write to
+     */
+    public static final String CONTEXT_PATH = "CONTEXT_PATH";
+
+
+    @Override
+    public void postHandle(final HttpServletRequest request,
+            final HttpServletResponse response, final Object handler,
+            final ModelAndView modelAndView) throws Exception {
+    	
+    	String contextPath = "";
+    	if (request.getSession(false) != null) {
+			contextPath = request.getContextPath();
+    	}
+    	
+        if (modelAndView != null) {
+            modelAndView.getModelMap().addAttribute(CONTEXT_PATH, contextPath);
+        }
+        
+    }
+    
+}
