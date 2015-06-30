02c423a4d18dd76a24a772cd57c249a566554434
jtmelton <jtmelton@gmail.com>
2015-06-29 21:55:00 -0400

    expose websocket endpoint for appsensor-ui

---
+++ appsensor-ui/src/main/java/org/owasp/appsensor/ui/websocket/WebSocketConfiguration.java
@@ -0,0 +1,17 @@
+package org.owasp.appsensor.ui.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/appsensor-websocket").withSockJS();
+    }
+
+}
