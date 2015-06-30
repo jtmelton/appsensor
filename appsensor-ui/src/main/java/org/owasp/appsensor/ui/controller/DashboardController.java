9ce29b35e73e717ec262ab839a1fc4b0d92b1a08
jtmelton <jtmelton@gmail.com>
2015-06-29 21:52:51 -0400

    add ping for dashboard

--- appsensor-ui/src/main/java/org/owasp/appsensor/ui/controller/DashboardController.java
+++ appsensor-ui/src/main/java/org/owasp/appsensor/ui/controller/DashboardController.java
@@ -3,6 +3,7 @@ package org.owasp.appsensor.ui.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DashboardController {
@@ -10,6 +11,12 @@ public class DashboardController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String home() {
 		return "dashboard";
+	}
+	
+	@RequestMapping(value="/ping", method = RequestMethod.GET)
+	@ResponseBody
+	public boolean keepalive() {
+		return true;
 	}
 
 }
