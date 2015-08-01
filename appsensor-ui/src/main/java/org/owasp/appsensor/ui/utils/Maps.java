541b6bae0a3024891d3deb7a2da8ea7df58c29b9
jtmelton <jtmelton@gmail.com>
2015-07-31 22:46:10 -0400

    Sorting maps by detection point date

--- appsensor-ui/src/main/java/org/owasp/appsensor/ui/utils/Maps.java
+++ appsensor-ui/src/main/java/org/owasp/appsensor/ui/utils/Maps.java
@@ -4,20 +4,11 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.owasp.appsensor.core.DetectionPoint;
-
 public class Maps {
 	
 	public static TreeMap<String, Long> sortStringsByValue(Map<String, Long> unsorted) {
 		StringComparator comparator =  new StringComparator(unsorted);
 		TreeMap<String,Long> sortedMap = new TreeMap<>(comparator);
-		sortedMap.putAll(unsorted);
-		return sortedMap;
-	}
-	
-	public static TreeMap<DetectionPoint, Long> sortDetectionPointsByValue(Map<DetectionPoint, Long> unsorted) {
-		DetectionPointComparator comparator =  new DetectionPointComparator(unsorted);
-		TreeMap<DetectionPoint,Long> sortedMap = new TreeMap<>(comparator);
 		sortedMap.putAll(unsorted);
 		return sortedMap;
 	}
@@ -31,23 +22,6 @@ public class Maps {
 	    }
 	 
 	    public int compare(String a, String b) {
-	        if (map.get(a) >= map.get(b)) {
-	            return -1;
-	        } else {
-	            return 1;
-	        } // returning 0 would merge keys 
-	    }
-	}
-	
-	static class DetectionPointComparator implements Comparator<DetectionPoint> {
-		 
-	    Map<DetectionPoint, Long> map;
-	 
-	    public DetectionPointComparator(Map<DetectionPoint, Long> base) {
-	        this.map = base;
-	    }
-	 
-	    public int compare(DetectionPoint a, DetectionPoint b) {
 	        if (map.get(a) >= map.get(b)) {
 	            return -1;
 	        } else {
