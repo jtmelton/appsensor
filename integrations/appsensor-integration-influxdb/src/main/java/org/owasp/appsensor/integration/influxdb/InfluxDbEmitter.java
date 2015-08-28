45ecf597b7f1621959feda143f2fde070b4d69af
jtmelton <jtmelton@gmail.com>
2015-08-27 21:11:34 -0400

    add influxdb inserts to emitter

--- integrations/appsensor-integration-influxdb/src/main/java/org/owasp/appsensor/integration/influxdb/InfluxDbEmitter.java
+++ integrations/appsensor-integration-influxdb/src/main/java/org/owasp/appsensor/integration/influxdb/InfluxDbEmitter.java
@@ -1,24 +1,19 @@
 package org.owasp.appsensor.integration.influxdb;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Named;
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.MBeanRegistrationException;
-import javax.management.MBeanServer;
-import javax.management.NotCompliantMBeanException;
-import javax.management.Notification;
-import javax.management.NotificationBroadcasterSupport;
-import javax.management.NotificationListener;
-import javax.management.ObjectName;
+import java.util.concurrent.TimeUnit;
 
+import javax.inject.Named;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
 import org.owasp.appsensor.core.Attack;
 import org.owasp.appsensor.core.Event;
 import org.owasp.appsensor.core.Response;
 import org.owasp.appsensor.core.listener.SystemListener;
 import org.owasp.appsensor.core.logging.Loggable;
+import org.owasp.appsensor.core.util.DateUtils;
 import org.slf4j.Logger;
-
-import com.google.gson.Gson;
 
 /**
  * This is the Influxdb Emitter. 
@@ -38,10 +33,16 @@ import com.google.gson.Gson;
 @Loggable
 public class InfluxDbEmitter extends SystemListener {
 	
-	@SuppressWarnings("unused")
 	private Logger logger;
 	
 	public InfluxDbEmitter() {}
+
+	private  InfluxDB db = InfluxDBFactory.connect("http://127.0.0.1:8086", "root", "root");
+	
+	private static final String DB = "appsensor";
+	private static final String EVENTS = "appsensor_events";
+	private static final String ATTACKS = "appsensor_attacks";
+	private static final String RESPONSES = "appsensor_responses";
 	
 	/**
 	 * {@inheritDoc}
@@ -51,21 +52,33 @@ public class InfluxDbEmitter extends SystemListener {
 		// this is the user that caused the event
 		String username = event.getUser().getUsername();
 		// ip address of user may or may not exist
-		if(event.getUser().getIPAddress() != null) {
-			String userIpAddress = event.getUser().getIPAddress().getAddressAsString();
-		}
+//		if(event.getUser().getIPAddress() != null) {
+//			String userIpAddress = event.getUser().getIPAddress().getAddressAsString();
+//		}
 		// timestamp when event occurred
 		String timestamp = event.getTimestamp();
 		// detection system (what application saw the event)
 		String detectionSystem = event.getDetectionSystem().getDetectionSystemId();
 		// detection system ip address may or may not exist
-		if(event.getDetectionSystem().getIPAddress() != null) {
-			String detectionSystemIpAddress = event.getDetectionSystem().getIPAddress().getAddressAsString();
-		}
+//		if(event.getDetectionSystem().getIPAddress() != null) {
+//			String detectionSystemIpAddress = event.getDetectionSystem().getIPAddress().getAddressAsString();
+//		}
 		// category for detection point 
 		String category = event.getDetectionPoint().getCategory();
 		// label for detection point
 		String label = event.getDetectionPoint().getLabel();
+		
+		Point point = Point.measurement(EVENTS)
+				.time(DateUtils.fromString(timestamp).getMillis(), TimeUnit.MILLISECONDS)
+				.field("label", label)
+				.tag("username",username)
+				.tag("timestamp",timestamp)
+				.tag("detectionSystem",detectionSystem)
+				.tag("category",category)
+				.tag("label",label)
+                .build();
+		
+		db.write(DB, "default", point);
 		
 		logger.info("received event in influx db emitter at: " + event.getTimestamp());
 	}
@@ -78,17 +91,17 @@ public class InfluxDbEmitter extends SystemListener {
 		// this is the user that caused the attack
 		String username = attack.getUser().getUsername();
 		// ip address of user may or may not exist
-		if(attack.getUser().getIPAddress() != null) {
-			String userIpAddress = attack.getUser().getIPAddress().getAddressAsString();
-		}
+//		if(attack.getUser().getIPAddress() != null) {
+//			String userIpAddress = attack.getUser().getIPAddress().getAddressAsString();
+//		}
 		// timestamp when attack occurred
 		String timestamp = attack.getTimestamp();
 		// detection system (what application saw the attack)
 		String detectionSystem = attack.getDetectionSystem().getDetectionSystemId();
 		// detection system ip address may or may not exist
-		if(attack.getDetectionSystem().getIPAddress() != null) {
-			String detectionSystemIpAddress = attack.getDetectionSystem().getIPAddress().getAddressAsString();
-		}
+//		if(attack.getDetectionSystem().getIPAddress() != null) {
+//			String detectionSystemIpAddress = attack.getDetectionSystem().getIPAddress().getAddressAsString();
+//		}
 		// category for detection point 
 		String category = attack.getDetectionPoint().getCategory();
 		// label for detection point
@@ -99,6 +112,21 @@ public class InfluxDbEmitter extends SystemListener {
 		int thresholdIntervalDuration = attack.getDetectionPoint().getThreshold().getInterval().getDuration();
 		// unit for detection point threshold interval
 		String thresholdIntervalUnit = attack.getDetectionPoint().getThreshold().getInterval().getUnit();
+		
+		Point point = Point.measurement(ATTACKS)
+				.time(DateUtils.fromString(timestamp).getMillis(), TimeUnit.MILLISECONDS)
+				.field("label", label)
+				.tag("username",username)
+				.tag("timestamp",timestamp)
+				.tag("detectionSystem",detectionSystem)
+				.tag("category",category)
+				.tag("label",label)
+				.tag("thresholdCount",String.valueOf(thresholdCount))
+				.tag("thresholdIntervalDuration",String.valueOf(thresholdIntervalDuration))
+				.tag("thresholdIntervalUnit",thresholdIntervalUnit)
+                .build();
+		
+		db.write(DB, "default", point);
 		
 		logger.info("received attack in influx db emitter at: " + attack.getTimestamp());
 	}
@@ -111,26 +139,37 @@ public class InfluxDbEmitter extends SystemListener {
 		// this is the user that the response should be executed on
 		String username = response.getUser().getUsername();
 		// ip address of user may or may not exist
-		if(response.getUser().getIPAddress() != null) {
-			String userIpAddress = response.getUser().getIPAddress().getAddressAsString();
-		}
+//		if(response.getUser().getIPAddress() != null) {
+//			String userIpAddress = response.getUser().getIPAddress().getAddressAsString();
+//		}
 		// timestamp when response was generated
 		String timestamp = response.getTimestamp();
 		// detection system (what application should perform the response)
 		String detectionSystem = response.getDetectionSystem().getDetectionSystemId();
 		// detection system ip address may or may not exist
-		if(response.getDetectionSystem().getIPAddress() != null) {
-			String detectionSystemIpAddress = response.getDetectionSystem().getIPAddress().getAddressAsString();
-		}
+//		if(response.getDetectionSystem().getIPAddress() != null) {
+//			String detectionSystemIpAddress = response.getDetectionSystem().getIPAddress().getAddressAsString();
+//		}
 		// the name of the actual response to execute
 		String action = response.getAction();
 		// response interval may or may not exist
-		if(response.getInterval() != null) {
-			// duration for response interval
-			int intervalDuration = response.getInterval().getDuration();
-			// unit for response interval
-			String intervalUnit = response.getInterval().getUnit();
-		}
+//		if(response.getInterval() != null) {
+//			// duration for response interval
+//			int intervalDuration = response.getInterval().getDuration();
+//			// unit for response interval
+//			String intervalUnit = response.getInterval().getUnit();
+//		}
+		
+		Point point = Point.measurement(RESPONSES)
+				.time(DateUtils.fromString(timestamp).getMillis(), TimeUnit.MILLISECONDS)
+				.field("action", action)
+				.tag("username",username)
+				.tag("timestamp",timestamp)
+				.tag("detectionSystem",detectionSystem)
+				.tag("action",action)
+                .build();
+		
+		db.write(DB, "default", point);
 		
 		logger.info("received response in influx db emitter at: " + response.getTimestamp());
 	}
