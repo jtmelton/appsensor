4ac6b9b49801dc98245a862191ae4a0551c82e86
jtmelton <jtmelton@gmail.com>
2015-09-04 22:03:53 -0400

    Updating JPA storage provider to use the JPA2 criteria api for searches to improve performance rather than a simpler (slower) model of pulling all results back and filtering in appsensor.

--- storage-providers/appsensor-storage-jpa2/src/main/java/org/owasp/appsensor/storage/jpa2/dao/EventRepository.java
+++ storage-providers/appsensor-storage-jpa2/src/main/java/org/owasp/appsensor/storage/jpa2/dao/EventRepository.java
@@ -1,11 +1,19 @@
 package org.owasp.appsensor.storage.jpa2.dao;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.owasp.appsensor.core.Event;
+import org.owasp.appsensor.core.criteria.SearchCriteria;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,6 +64,87 @@ public class EventRepository {
 	@Transactional(readOnly = true)
 	public Collection<Event> findAll() {
 		return em.createQuery("FROM Event", Event.class).getResultList();
+	}
+	
+	/**
+	 * Retrive all {@link Event}s from the DB
+	 * 
+	 * @return {@link Collection} of {@link Event}s from the DB
+	 */
+	@Transactional(readOnly = true)
+	public Collection<Event> find(SearchCriteria searchCriteria) {
+		
+//		if user exists, compare on [username]
+//		if detection point exists, compare on [category, label, threshold count, threshold interval]
+//		if detection system ids exist, do an "in"
+//		if earliest exists, compare on [event date > earliest]
+		
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Event> criteriaQuery = criteriaBuilder.createQuery(Event.class);
+		Root<Event> root = criteriaQuery.from(Event.class);
+		
+		Collection<Predicate> conditions = new ArrayList<>();
+		
+		if (searchCriteria.getUser() != null) {
+			Predicate userCondition = criteriaBuilder.equal(root.get("user").get("username"), searchCriteria.getUser().getUsername());
+			conditions.add(userCondition);
+		}
+		
+		if (searchCriteria.getDetectionPoint() != null) {
+			
+			if (searchCriteria.getDetectionPoint().getCategory() != null) {
+				Predicate categoryCondition = criteriaBuilder.equal(root.get("detectionPoint").get("category"), 
+						searchCriteria.getDetectionPoint().getCategory());
+				conditions.add(categoryCondition);
+			}
+			
+			if (searchCriteria.getDetectionPoint().getLabel() != null) {
+				Predicate labelCondition = criteriaBuilder.equal(root.get("detectionPoint").get("label"), 
+						searchCriteria.getDetectionPoint().getLabel());
+				conditions.add(labelCondition);
+			}
+			
+			if (searchCriteria.getDetectionPoint().getThreshold() != null) {
+				
+				if (searchCriteria.getDetectionPoint().getThreshold().getCount() > 0) {
+					Predicate countCondition = criteriaBuilder.equal(root.get("detectionPoint").get("threshold").get("count"), 
+							searchCriteria.getDetectionPoint().getThreshold().getCount());
+					conditions.add(countCondition);
+				}
+				
+				if (searchCriteria.getDetectionPoint().getThreshold().getInterval() != null) {
+					if (searchCriteria.getDetectionPoint().getThreshold().getInterval().getUnit() != null) {
+						Predicate durationCondition = criteriaBuilder.equal(root.get("detectionPoint").get("threshold").get("interval").get("duration"), 
+								searchCriteria.getDetectionPoint().getThreshold().getInterval().getDuration());
+						conditions.add(durationCondition);
+					}
+					
+					if (searchCriteria.getDetectionPoint().getThreshold().getInterval().getDuration() > 0) {
+						Predicate unitCondition = criteriaBuilder.equal(root.get("detectionPoint").get("threshold").get("interval").get("unit"), 
+								searchCriteria.getDetectionPoint().getThreshold().getInterval().getUnit());
+						conditions.add(unitCondition);
+					}
+				}
+				
+			}
+			
+		}
+		
+		if (searchCriteria.getDetectionSystemIds() != null) {
+			Predicate detectionSystemCondition = root.get("detectionSystem").get("detectionSystemId").in(searchCriteria.getDetectionSystemIds());
+			conditions.add(detectionSystemCondition);
+		}
+
+		if (conditions.size() > 0) {
+			criteriaQuery.where(criteriaBuilder.and(conditions.toArray(new Predicate[0])));
+		}
+		
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("timestamp")));
+
+		TypedQuery<Event> query = em.createQuery(criteriaQuery); 
+		List<Event> result = query.getResultList();
+		
+		return result;
 	}
 	
 }
--- storage-providers/appsensor-storage-jpa2/src/main/java/org/owasp/appsensor/storage/jpa2/dao/ResponseRepository.java
+++ storage-providers/appsensor-storage-jpa2/src/main/java/org/owasp/appsensor/storage/jpa2/dao/ResponseRepository.java
@@ -1,11 +1,21 @@
 package org.owasp.appsensor.storage.jpa2.dao;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
+import org.owasp.appsensor.core.Attack;
 import org.owasp.appsensor.core.Response;
+import org.owasp.appsensor.core.criteria.SearchCriteria;
+import org.owasp.appsensor.core.util.DateUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,6 +66,41 @@ public class ResponseRepository {
 	@Transactional(readOnly = true)
 	public Collection<Response> findAll() {
 		return em.createQuery("FROM Response", Response.class).getResultList();
+	}
+	
+	/**
+	 * Retrive all {@link Response}s from the DB matching criteria
+	 * 
+	 * @return {@link Collection} of {@link Response}s from the DB
+	 */
+	@Transactional(readOnly = true)
+	public Collection<Response> find(SearchCriteria searchCriteria) {
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Response> criteriaQuery = criteriaBuilder.createQuery(Response.class);
+		Root<Response> root = criteriaQuery.from(Response.class);
+		
+		Collection<Predicate> conditions = new ArrayList<>();
+		
+		if (searchCriteria.getUser() != null) {
+			Predicate userCondition = criteriaBuilder.equal(root.get("user").get("username"), searchCriteria.getUser().getUsername());
+			conditions.add(userCondition);
+		}
+		
+		if (searchCriteria.getDetectionSystemIds() != null) {
+			Predicate detectionSystemCondition = root.get("detectionSystem").get("detectionSystemId").in(searchCriteria.getDetectionSystemIds());
+			conditions.add(detectionSystemCondition);
+		}
+
+		if (conditions.size() > 0) {
+			criteriaQuery.where(criteriaBuilder.and(conditions.toArray(new Predicate[0])));
+		}
+		
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("timestamp")));
+
+		TypedQuery<Response> query = em.createQuery(criteriaQuery); 
+		List<Response> result = query.getResultList();
+		
+		return result;
 	}
 	
 }
