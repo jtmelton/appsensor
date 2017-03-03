19014872685173b10eec77f56f8d064825558ce7
John Melton <jtmelton@gmail.com>
2017-03-03 12:52:13 -0500

    fix typo

--- appsensor-ui/src/main/java/org/owasp/appsensor/AppsensorUiApplication.java
+++ appsensor-ui/src/main/java/org/owasp/appsensor/AppsensorUiApplication.java
@@ -59,7 +59,7 @@ public class AppsensorUiApplication {
         public void customize(ConfigurableEmbeddedServletContainer container) {
         	ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/400.html");
             ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-	    ErrorPage error403Page = new ErrorPage(Httpstatus.FORBIDDEN, "/403.html");
+	    ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/403.html");
             ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
             ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
  
