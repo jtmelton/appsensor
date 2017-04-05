4859bd7332b1f6840873b994f6bda6f0c3b0a89d
jtmelton <jtmelton@gmail.com>
2017-04-05 02:05:51 -0400

    adding new sample app to cover rest / websocket / mysql combo - requested by an end user

--- integrations/appsensor-integration-spring-security/src/main/java/org/owasp/appsensor/integration/springsecurity/response/SpringSecurityUserManager.java
+++ integrations/appsensor-integration-spring-security/src/main/java/org/owasp/appsensor/integration/springsecurity/response/SpringSecurityUserManager.java
@@ -1,142 +1,143 @@
 package org.owasp.appsensor.integration.springsecurity.response;
 
 import java.util.Collection;
-
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import org.owasp.appsensor.core.User;
 import org.owasp.appsensor.core.logging.Loggable;
 import org.owasp.appsensor.core.response.UserManager;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 /**
  * This is an adapter for Spring Security to do user management actions
- * as part of the response 
+ * as part of the response
  *
  * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
  */
 // primary - use this when on classpath (overrides other user manager impls
-@Primary	
+@Primary
 @Named
 @Loggable
-public class SpringSecurityUserManager implements UserManager {
+public class SpringSecurityUserManager extends GlobalAuthenticationConfigurerAdapter implements
+    UserManager {
 
-	private Logger logger;
-	
-	@Inject
-	private AuthenticationManagerBuilder builder;
-	
-	@Inject
-	private UserResponseCache userResponseCache;
-	
-	private UserDetailsManager userDetailsManager;
-	
-	@PostConstruct
-	public void initialize() {
-		if(builder != null && 
-				builder.getDefaultUserDetailsService() != null &&
-				builder.getDefaultUserDetailsService() instanceof UserDetailsManager) {
-			userDetailsManager = (UserDetailsManager)builder.getDefaultUserDetailsService();
-		}
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void logout(User appSensorUser) {
-		logger.info("Request received to logout user <{}>.", appSensorUser.getUsername());
-		
-		userResponseCache.setUserLoggedOut(appSensorUser.getUsername());
-	}
+    private Logger logger;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void disable(User appSensorUser) {
-		logger.info("Request received to disable user <{}>.", appSensorUser.getUsername());
-		
-		if (userDetailsManager == null) {
-			logger.warn("Could not disable user [" + appSensorUser.getUsername() + "] since the spring security " +
-					"UserDetailsManager is not properly configured." );
-			return;
-		}
-		
-		UserDetails springUser = userDetailsManager.loadUserByUsername(appSensorUser.getUsername());
-		
-		if (springUser == null) {
-			logger.warn("Could not disable user [" + appSensorUser.getUsername() + "] because the user could not " +
-					"be found by lookup" );
-			return;
-		}
-		
-		logger.info("Disabling user <{}>.", springUser.getUsername());
-		userDetailsManager.updateUser(new DisabledUser(springUser));
-		
-		logger.info("After disabling user <{}>, also logging out so the disable gets triggered.", springUser.getUsername());
-		userResponseCache.setUserLoggedOut(appSensorUser.getUsername());
-	}
-	
-	/**
-	 * Simple shim helper class to represent disabled user. 
-	 * Delegate all methods except for #isEnabled
-	 * 
-	 * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
-	 */
-	public static class DisabledUser implements UserDetails {
+    @Inject
+    private UserResponseCache userResponseCache;
 
-		private static final long serialVersionUID = 9173900190245012681L;
+    private UserDetailsManager userDetailsManager;
 
-		private UserDetails delegate;
-		
-		public DisabledUser(UserDetails userDetails) {
-			this.delegate = userDetails;
-		}
-		
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			return delegate.getAuthorities();
-		}
+    @Override
+    public void init(AuthenticationManagerBuilder auth) {
+        if (auth != null &&
+            auth.getDefaultUserDetailsService() != null &&
+            auth.getDefaultUserDetailsService() instanceof UserDetailsManager) {
+            userDetailsManager = (UserDetailsManager) auth.getDefaultUserDetailsService();
+        } else {
+            throw new IllegalStateException("UserDetailsManager could not be constructed properly");
+        }
+    }
 
-		@Override
-		public String getPassword() {
-			return delegate.getPassword();
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void logout(User appSensorUser) {
+        logger.info("Request received to logout user <{}>.", appSensorUser.getUsername());
 
-		@Override
-		public String getUsername() {
-			return delegate.getUsername();
-		}
+        userResponseCache.setUserLoggedOut(appSensorUser.getUsername());
+    }
 
-		@Override
-		public boolean isAccountNonExpired() {
-			return delegate.isAccountNonExpired();
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void disable(User appSensorUser) {
+        logger.info("Request received to disable user <{}>.", appSensorUser.getUsername());
 
-		@Override
-		public boolean isAccountNonLocked() {
-			return delegate.isAccountNonLocked();
-		}
+        if (userDetailsManager == null) {
+            logger.warn("Could not disable user [" + appSensorUser.getUsername()
+                        + "] since the spring security " +
+                        "UserDetailsManager is not properly configured.");
+            return;
+        }
 
-		@Override
-		public boolean isCredentialsNonExpired() {
-			return delegate.isCredentialsNonExpired();
-		}
+        UserDetails springUser = userDetailsManager.loadUserByUsername(appSensorUser.getUsername());
 
-		@Override
-		public boolean isEnabled() {
-			// override this one specifically - we are disabling the user
-			return false;
-		}
-		
-	}
-	
+        if (springUser == null) {
+            logger.warn("Could not disable user [" + appSensorUser.getUsername()
+                        + "] because the user could not " +
+                        "be found by lookup");
+            return;
+        }
+
+        logger.info("Disabling user <{}>.", springUser.getUsername());
+        userDetailsManager.updateUser(new DisabledUser(springUser));
+
+        logger.info("After disabling user <{}>, also logging out so the disable gets triggered.",
+            springUser.getUsername());
+        userResponseCache.setUserLoggedOut(appSensorUser.getUsername());
+    }
+
+    /**
+     * Simple shim helper class to represent disabled user.
+     * Delegate all methods except for #isEnabled
+     *
+     * @author John Melton (jtmelton@gmail.com) http://www.jtmelton.com/
+     */
+    public static class DisabledUser implements UserDetails {
+
+        private static final long serialVersionUID = 9173900190245012681L;
+
+        private UserDetails delegate;
+
+        public DisabledUser(UserDetails userDetails) {
+            this.delegate = userDetails;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return delegate.getAuthorities();
+        }
+
+        @Override
+        public String getPassword() {
+            return delegate.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return delegate.getUsername();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return delegate.isAccountNonExpired();
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return delegate.isAccountNonLocked();
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return delegate.isCredentialsNonExpired();
+        }
+
+        @Override
+        public boolean isEnabled() {
+            // override this one specifically - we are disabling the user
+            return false;
+        }
+
+    }
+
 }
